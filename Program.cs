e3789e19122d6a1397aaf2c92b09bdb48a9739ba
shivohum04 <shivohum04@gmail.com>
2024-01-19 21:31:56 +0530

    [shiv] ADD:added the solution for uc2

--- Program.cs
+++ Program.cs
@@ -1,3 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace uc1
+{
+    class Program
+    {
+        static void checkAttendance(int emp)
+        {
+            if (emp == 1)
+            {
+                Console.WriteLine("employee is here");
+
+            }
+            else
+            {
+                Console.WriteLine("employee absent");
+
+            }
+        }
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int value = random.Next(0, 2);
+            checkAttendance(value);
+
+            int wph = 20; // work per hour 
+            int hpd = 8;// hours in a day 
+            int wpd = wph * hpd;// daily wage
+            Console.Write("the daily wage is ");
+            Console.WriteLine(wpd);
+            Console.ReadLine();
+        }
+
+    }
+}
