6fdd75a89da6416ed2745beca8d49876f1d6dc37
shivohum04 <shivohum04@gmail.com>
2024-01-19 21:04:59 +0530

    [shiv]ADD: added solution to uc1

--- Program.cs
+++ Program.cs
@@ -1,3 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace Hello
+{
+    class Program
+    {
+        static void checkAttendance(int emp)
+        {
+            if (emp == 1)
+            {
+                Console.WriteLine("employee is here");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("employee absent");
+                Console.ReadLine();
+            }
+        }
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int value = random.Next(0, 2);
+            checkAttendance(value);
+        }
+    }
+}
